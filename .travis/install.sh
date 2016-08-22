7b5fb451d37e9352609b57cb7b0a0ed2154adb47
Brian Wilcox <bwilcox@linkedin.com>
2016-08-22 07:43:46 -0700

    More verbose output for install.sh and env vars in .travis.yml

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Installing dependencies for '${TRAVIS_PYTHON_VERSION}'"
 if [[ ${TRAVIS_PYTHON_VERSION} == 2* ]]; then
     pip install -r requirements-python2.txt
 fi
@@ -7,4 +8,5 @@ if [[ ${TRAVIS_PYTHON_VERSION} == 3* ]]; then
     pip install -r requirements-python3.txt
 fi
 
+echo "Installing test requirements"
 pip install -r .travis/requirements-test.txt
